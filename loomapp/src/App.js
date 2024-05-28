3dd0758c2cac391260e91d7dd66a09278805da2d
Joe Delfino <jdelfino@gmail.com>
2024-05-28 10:26:53 -0400

    UI updates, including load twill/plain buttons

--- loomapp/src/App.js
+++ loomapp/src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import { useState } from 'react';
-import {ButtonGroup, Button, Container, Row, SplitButton, Dropdown} from 'react-bootstrap';
+import {ButtonGroup, Button, Container, Row} from 'react-bootstrap';
+import { IoReload } from "react-icons/io5";
+import { MdContentPasteGo } from "react-icons/md";
+import { MdContentCopy } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
+import { RiDeleteBack2Fill } from "react-icons/ri";
+import { MdHourglassBottom } from "react-icons/md";
+import { MdSend } from "react-icons/md";
+
+function emptyPattern() {
+  return Array(20).fill().map(() => Array(40).fill(0));
+}
 
 function App() {
   // 40 x 40 array, initialize to all 0s
-  const [pattern, setPattern] = useState(Array(20).fill(Array(40).fill(0)));
+  const [pattern, setPattern] = useState(emptyPattern());
   const [sendState, setSendState] = useState("unsent");
 
   function send(i) {
     (async () => {
       setSendState("sending");
       console.log("sending ", i);
+      await new Promise(r => setTimeout(r, 1000));
+      setSendState("sent");
+
+      /*
       fetch('/api/setrow', {
           method: 'POST',
           headers: {
@@ -25,6 +40,7 @@ function App() {
           console.log("got response", data);
           setSendState("sent");
         });
+        */
     })();
   }
 
@@ -79,12 +95,10 @@ function App() {
   }
 
   function copyRow(i) {
-    console.log("copy", i, pattern[i]);
     navigator.clipboard.writeText(pattern[i].join(""));
 
   }
   function pasteRow(i) {
-    console.log("pasting to ", i);
     navigator.clipboard.readText().then(text => {
       const newPattern = pattern.map((row, k) => {
         if (k === i) {
@@ -97,6 +111,22 @@ function App() {
     });
   }
 
+  function loadPattern(cycle) {
+    let offset = 0;
+    let newPattern = emptyPattern();
+    for (let i = 0; i < pattern.length; i++) {
+      for (let j = 0; j < pattern[i].length; j++) {
+        if ((j - offset) % cycle === 0) {
+          newPattern[i][j] = 0;
+        } else {
+          newPattern[i][j] = 1;
+        }
+      }
+      offset += 1;
+    }
+    setPattern(newPattern);
+  }
+
   const rows = pattern.map((row, i) => {
     let cl = () => { clearRow(i);}
     let rm = () => { removeRow(i);}
@@ -104,71 +134,43 @@ function App() {
     let ps = () => { pasteRow(i);}
     let sn = () => { send(i);}
 
+    function copyButton() {
+      return <Button variant="primary" size="sm" title="Copy" onClick={cp}><MdContentCopy /></Button>
+    }
+    function pasteButton() {
+      return <Button variant="primary" size="sm" title="Paste" onClick={ps}><MdContentPasteGo /></Button>
+    }
+    function clearButton() {
+      return <Button variant="danger" size="sm" title="Clear" onClick={cl}><RiDeleteBack2Fill /></Button>
+    }
+    function deleteRowButton() {
+      return <Button variant="danger" size="sm" title="Clear" onClick={rm}><MdDeleteForever /></Button>
+    }
+    function reloadButton() {
+      return <Button variant="success" size="sm" title="Resend" onClick={sn}><IoReload /></Button>
+    }
+    function sendButton() {
+      return <Button variant="success" size="sm" title="Send" onClick={sn}><MdSend /></Button>
+    }
+    function sendingButton() {
+      return <Button variant="primary" size="sm" title="Sending" disabled><MdHourglassBottom /></Button>
+    }
+  
     function getActions() {
+      let buttons = []
       if (i !== 0) {
-        return (
-          <SplitButton
-            as={ButtonGroup}
-            key={"bg-" + i}
-            variant="danger"
-            title="Clear"
-            onClick={cl}
-          >
-            <Dropdown.Item eventKey="1"><div onClick={cl}>Clear</div></Dropdown.Item>
-            <Dropdown.Item eventKey="2"><div onClick={rm}>Remove</div></Dropdown.Item>
-            <Dropdown.Item eventKey="3"><div onClick={cp}>Copy</div></Dropdown.Item>
-            <Dropdown.Item eventKey="4"><div onClick={ps}>Paste</div></Dropdown.Item>
-          </SplitButton>
-        );        
-      } else if (sendState === "sent") {
-        return (
-          <SplitButton
-            as={ButtonGroup}
-            key={"bg-" + i}
-            variant="success"
-            title="Remove"
-            size="sm"
-            onClick={rm}
-          >
-            <Dropdown.Item eventKey="1"><div onClick={cl}>Clear</div></Dropdown.Item>
-            <Dropdown.Item eventKey="2"><div onClick={rm}>Remove</div></Dropdown.Item>
-            <Dropdown.Item eventKey="3"><div onClick={cp}>Copy</div></Dropdown.Item>
-            <Dropdown.Item eventKey="4"><div onClick={ps}>Paste</div></Dropdown.Item>
-          </SplitButton>
-        );
-      } else if (sendState === "sending") {
-        return (
-          <SplitButton
-            as={ButtonGroup}
-            key={"bg-" + i}
-            variant="success"
-            disabled
-            title="(Sending)"
-            size="sm"
-            onClick={rm}
-          >
-            <Dropdown.Item disabled eventKey="1"><div onClick={cl}>Clear</div></Dropdown.Item>
-            <Dropdown.Item disabled eventKey="2"><div onClick={rm}>Remove</div></Dropdown.Item>
-            <Dropdown.Item disabled eventKey="3"><div onClick={cp}>Copy</div></Dropdown.Item>
-            <Dropdown.Item disabled eventKey="4"><div onClick={ps}>Paste</div></Dropdown.Item>
-          </SplitButton>
-        );        
+          buttons.push(copyButton(), pasteButton(), clearButton(), deleteRowButton());
       } else {
-        return (
-          <SplitButton
-            as={ButtonGroup}
-            key={"bg-" + i}
-            variant="success"
-            title="Send"
-            onClick={sn}
-          >
-            <Dropdown.Item eventKey="1"><div onClick={cl}>Clear</div></Dropdown.Item>
-            <Dropdown.Item eventKey="2"><div onClick={rm}>Remove</div></Dropdown.Item>
-            <Dropdown.Item eventKey="3"><div onClick={cp}>Copy</div></Dropdown.Item>
-            <Dropdown.Item eventKey="4"><div onClick={ps}>Paste</div></Dropdown.Item>
-          </SplitButton>  
-        );      
+        if (sendState === "sent") {
+          buttons.push(reloadButton());
+        } else if (sendState === "sending") {
+          buttons.push(sendingButton());
+        } else {
+          buttons.push(sendButton());
+        }
+        buttons.push(copyButton(), pasteButton(), deleteRowButton());
       }
+      return <ButtonGroup>{buttons}</ButtonGroup>
     }
 
     return (
@@ -186,7 +188,6 @@ function App() {
           })
         }
         {getActions()}
-
       </ButtonGroup>
       </Row>
     );
@@ -194,6 +195,11 @@ function App() {
 
   return (
     <Container fluid>
+      <div>
+          <Button variant="info" size="sm" title="Load Twill" onClick={() => loadPattern(2)}>Load Plain Weave</Button>
+          <Button variant="info" size="sm" title="Load Twill" onClick={() => loadPattern(3)}>Load 2:1 Twill</Button>
+          <Button variant="info" size="sm" title="Load Twill" onClick={() => loadPattern(4)}>Load 3:1 Twill</Button>
+      </div>
       {rows}
     </Container>
   );
